58013b2d6271cc6e00d281b5b8141def8f19dd4b
Alex van Vliet <alex.van-vliet@epita.fr>
2019-08-06 20:32:01 +0200

    refactor(canasta): create sass components

--- src/components/canasta/canasta-view.js
+++ src/components/canasta/canasta-view.js
@@ -3,67 +3,70 @@ import React from 'react';
 import './canasta.sass';
 
 function CanastaView() {
-  return (<div className={'bg-blue-100 h-full'}>
-    <div className={'w-full mb-2 flex justify-center border-b-2'}>
-      <h1 className={'text-2xl font-bold py-2'}>Canasta</h1>
+  return <div className="canasta">
+    <div className="row header">
+      <h1 className="title">Canasta</h1>
     </div>
-    <div className={'w-full mb-2 flex'}>
-      <div className={'text-xl ml-2'}>Test 1</div>
-      <div className={'flex-grow'}></div>
-      <div className={'text-xl font-bold'}>0</div>
-      <div className={'text-center text-xl mx-2'}>VS</div>
-      <div className={'text-xl font-bold'}>0</div>
-      <div className={'flex-grow'}></div>
-      <div className={'text-xl mr-2'}>Test 2</div>
+    <div className="row">
+      <div className="team-name">Test 1</div>
+      <div className="spacer"/>
+      <div className="total-score">0</div>
+      <div className="vs-sign">VS</div>
+      <div className="total-score">0</div>
+      <div className="spacer"/>
+      <div className="team-name">Test 2</div>
     </div>
-    <div className={'w-full mb-2 flex'}>
-      <div>
-        <button
-            className={'text-white bg-blue-500 py-2 px-4 rounded-r hover:bg-blue-100 hover:text-blue-500 border border-blue-500'}>Red
-          Three
-        </button>
-      </div>
-      <div className={'flex-grow'}></div>
+    <div className="row">
+      <button className="button">Perfect Deal</button>
+      <div className="spacer"/>
       <div>0</div>
-      <div className={'text-xl mx-4'}></div>
+      <div className="spacer mid"/>
       <div>0</div>
-      <div className={'flex-grow'}></div>
-      <div>
-        <button
-            className={'text-white bg-blue-500 py-2 px-4 rounded-l hover:bg-blue-100 hover:text-blue-500 border border-blue-500'}>Red
-          Three
-        </button>
-      </div>
+      <div className="spacer"/>
+      <button className="button">Perfect Deal</button>
     </div>
-    <div>
-      <div>
-        <button>Fake Canasta</button>
-      </div>
+    <div className="row">
+      <button className="button">Red Three</button>
+      <div className="spacer"/>
       <div>0</div>
+      <div className="spacer mid"/>
       <div>0</div>
-      <div>
-        <button>Fake Canasta</button>
-      </div>
+      <div className="spacer"/>
+      <button className="button">Red Three</button>
     </div>
-    <div>
-      <div>
-        <button>Real Canasta</button>
-      </div>
+    <div className="row">
+      <button className="button">Mixed Canasta</button>
+      <div className="spacer"/>
       <div>0</div>
+      <div className="spacer mid"/>
       <div>0</div>
-      <div>
-        <button>Real Canasta</button>
-      </div>
+      <div className="spacer"/>
+      <button className="button">Mixed Canasta</button>
     </div>
-    <div>
-      <div>
-        <button>Out</button>
-      </div>
-      <div>
-        <button>Out</button>
-      </div>
+    <div className="row">
+      <button className="button">Natural Canasta</button>
+      <div className="spacer"/>
+      <div>0</div>
+      <div className="spacer mid"/>
+      <div>0</div>
+      <div className="spacer"/>
+      <button className="button">Natural Canasta</button>
     </div>
-  </div>);
+    <div className="row">
+      <button className="button">Go Out</button>
+      <div className="spacer"/>
+      <div className="spacer mid"/>
+      <div className="spacer"/>
+      <button className="button">Go Out</button>
+    </div>
+    <div className="row">
+      <div className="spacer"/>
+      <div className="total-score">0</div>
+      <div className="vs-sign">- 1 -</div>
+      <div className="total-score">0</div>
+      <div className="spacer"/>
+    </div>
+  </div>;
 }
 
 export default CanastaView;
