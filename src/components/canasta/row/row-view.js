d2aaea95df3a949286861914b79d798e43b4f2bf
Alex van Vliet <alex.van-vliet@epita.fr>
2019-08-07 00:32:48 +0200

    feat(canasta): center scores

--- src/components/canasta/row/row-view.js
+++ src/components/canasta/row/row-view.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function RowView({title, values, action=(team) => {}}) {
   return <div className="row">
-    <div className="spacer row">
+    <div className="spacer row centered">
       <button className="button left" onClick={() => action(0)}>{title}</button>
       <div className="spacer"/>
       <div>{values[0]}</div>
     </div>
     <div className="spacer mid"/>
-    <div className="spacer row">
+    <div className="spacer row centered">
       <div>{values[1]}</div>
       <div className="spacer"/>
       <button className="button right" onClick={() => action(1)}>{title}</button>
